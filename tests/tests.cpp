0bf9859ef6d62c66785f13d2ef6d98bdcedecb7a
Djordje Nedic <nedic.djordje2@gmail.com>
2022-08-23 19:54:48 +0200

    Add one more test

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -63,3 +63,41 @@ TEST_CASE("Write with overflow condition", "[write_overflow]") {
   REQUIRE(read_available == sizeof(test_data2));
   REQUIRE(memcmp(test_data2, read_location, sizeof(test_data2)) == 0);
 }
+
+TEST_CASE("Read data written after overflow condition write", "[read_after_overflow_write]") {
+  uint8_t buf[512];
+  const uint8_t test_data[320] = {0xE5U};
+
+  LFBB_Inst_Type lfbb;
+  LFBB_Init(&lfbb, buf, sizeof(buf));
+
+  /* Write to the start and read the data back */
+  uint8_t *write_location = LFBB_WriteAcquire(&lfbb, sizeof(test_data));
+  memcpy(write_location, test_data, sizeof(test_data));
+  LFBB_WriteRelease(&lfbb, sizeof(test_data));
+
+  size_t read_available;
+  uint8_t *read_location = LFBB_ReadAcquire(&lfbb, &read_available);
+  LFBB_ReadRelease(&lfbb, sizeof(test_data));
+
+  /* Write again, this time the oveflow will trigger and should give us the
+   * beginning again */
+  const uint8_t test_data2[240] = {0xA3U};
+  write_location = LFBB_WriteAcquire(&lfbb, sizeof(test_data2));
+  memcpy(write_location, test_data2, sizeof(test_data2));
+  LFBB_WriteRelease(&lfbb, sizeof(test_data2));
+
+  read_location = LFBB_ReadAcquire(&lfbb, &read_available);
+  LFBB_ReadRelease(&lfbb, sizeof(test_data2));
+
+  /* Write again, without overflow and read back */
+  const uint8_t test_data3[120] = {0xBCU};
+  write_location = LFBB_WriteAcquire(&lfbb, sizeof(test_data3));
+  memcpy(write_location, test_data3, sizeof(test_data3));
+  LFBB_WriteRelease(&lfbb, sizeof(test_data3));
+
+  read_location = LFBB_ReadAcquire(&lfbb, &read_available);
+  REQUIRE(read_location != nullptr);
+  REQUIRE(read_available == sizeof(test_data3));
+  REQUIRE(memcmp(test_data3, read_location, sizeof(test_data3)) == 0);
+}
