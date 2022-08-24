afc42340b3fdba92219e41d1165281d2e6ad61f4
Djordje Nedic <nedic.djordje2@gmail.com>
2022-08-24 15:38:36 +0200

    Bump version

--- lfbb/inc/lfbb.h
+++ lfbb/inc/lfbb.h
@@ -4,7 +4,7 @@
  * c11 suitable for both low-end microcontrollers all the way
  * to HPC machines. Lock-free for single consumer single
  * producer scenarios.
- * @version	1.0.1
+ * @version	1.0.2
  * @date 24. August 2022
  * @author Djordje Nedic
  **************************************************************/
@@ -37,7 +37,7 @@
  * This file is part of LFBB - Lock Free Bipartite Buffer
  *
  * Author:          Djordje Nedic <nedic.djordje2@gmail.com>
- * Version:         v1.0.1
+ * Version:         v1.0.2
  **************************************************************/
 
 /************************** INCLUDE ***************************/
